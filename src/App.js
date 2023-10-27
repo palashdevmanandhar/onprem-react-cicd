f8ad03743a083de1a3a1442dd0cdc5ef6a52068f
Palash Dev Manandhar <palash.manandhar@pdc01.lisnepal.com.np>
2023-10-27 15:42:30 +0545

    docker compose file included in the compose command

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. App built every 15 minutes
+          Edit <code>src/App.js</code> and save to reload. App built every 20 minutes
         </p>
         <a
           className="App-link"
